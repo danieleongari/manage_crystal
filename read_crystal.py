1f9cb12467285b4750c585875bf0bc29408581f3
Daniele Ongari <daniele.ongari@gmail.com>
2016-11-07 22:43:40 +0100

    Structured CELL<>ABC and fract<>xyz. Added input cssr, output xyz(CELL)

--- read_crystal.py
+++ read_crystal.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 """/
-Python program to read coordinates from a file and handle them
-
-USE: $ read_convert.py inputfile.xxx outputfile.yyy
-
-implemented already:
-xxx=sxyz    (next to implement cp2ki-restart, xsf, pwo, pwi, gaussian)
-yyy=cif,pdb
+Python program to read coordinates from a file and handle them. Daniele Ongari 7/11/16
 
 inputfilename          name of the input filename.inpformat
 inputformat	       input format
-
 
 natoms                 number of atoms
 ABC[012]               A B C in angstrom
@@ -20,11 +13,9 @@ cell(3x3)              unit cell matrix in angstrom
 
   atom[index]          atomic name
     an[index]          atomic number
-   xyz[index][x,y,z]   coordinates in angstrom
+   xyz[index][x,y,z]   cartesian coordinates in angstrom
  fract[index][x,y,z]   fractional coordinates
 charge[index]          partial charge
-
-
 
 """
 
@@ -41,8 +32,8 @@ if sys.argv[1]=='-h' or sys.argv[1]=='-help' or sys.argv[1]=='help':
 	print '#'
 	print '#  $ %s inputfile.xxx outputfile.yyy z' % (sys.argv[0])
 	print '#'
-	print '#  xxx=xyz(CELL),pdb  (next to implement cp2k-restart, xsf, pwo, pwi, gaussian, dcd+atoms)'
-	print '#  yyy=cif,pdb,cssr'
+	print '#  xxx=xyz(CELL),pdb,cssr          (next: cp2k-restart, xsf, pwo, pwi, gaussian, dcd+atoms)'
+	print '#  yyy=cif,pdb,cssr,xyz(CELL)'
 	print '#  z=f,l (for the first or the last coordinate in a dcd or pwo or axsf or log)'
         print '####################################################################################'
 	print
@@ -140,6 +131,29 @@ if inputformat=='pdb':
 		i=i+1	
 	 natoms=i
 
+if inputformat=='cssr':
+	line = file.readline()
+ 	celldim=line.split( )
+	ABC=[float(celldim[0]),float(celldim[1]),float(celldim[2])]
+	line = file.readline()
+ 	celldim=line.split( )
+	abc=[math.radians(float(celldim[0])),math.radians(float(celldim[1])),math.radians(float(celldim[2]))]
+	line = file.readline()
+        natoms=int(line.split( )[0])
+        line = file.readline() 
+        atom=[]
+        an=[]
+        fract=[]
+        charge=[]
+	i=0
+	for i in range(0,natoms):
+		line = file.readline()
+		data = line.split( )
+		atom.append(data[1])	
+		an.append(atom_tuple.index(atom[i]))
+                atom_count[an[i]]+=1
+		fract.append([float(data[2]), float(data[3]), float(data[4])])
+		charge.append(0.000)
 
 if inputformat=='xyz':
 	#read number of atoms
@@ -174,25 +188,37 @@ if inputformat=='xyz':
 		charge.append(0.000)
 
 ############################################################################# DO SOMETHING
-#make uc matrix if it was read in ABC+abc
-try:
-	cell
-except NameError:
-		print
-		print " ...converting CELL (ABCabc) to cell (matrix) "
-		#alpha=B^C, beta=A^C, gamma=A^B
-		cell=numpy.matrix([[                ABC[0],                     0.0,                                                                           0.0],
-		                   [ABC[1]*math.cos(abc[2]),ABC[1]*math.sin(abc[2]),                                                                           0.0],
-		                   [ABC[2]*math.cos(abc[1]),ABC[2]*math.cos(abc[0]),math.sqrt(ABC[2]**2-(ABC[2]*math.cos(abc[1]))**2-(ABC[2]*math.cos(abc[0]))**2)]]) #check this part
 
-#make uc ABC+abc if it was read in cell
- #todo
+if 'cell' in locals():   #make uc ABC+abc if it was read in cell
+  print
+  print " ...converting cell (matrix) to CELL (ABCabc)"
+  next=todo
+elif 'ABC' in locals():  #make uc matrix if it was read in ABC+abc
+  print
+  print " ...converting CELL (ABCabc) to cell (matrix) "
+  #alpha=B^C, beta=A^C, gamma=A^B
+  cell=numpy.matrix([[                ABC[0],                     0.0,                                                                           0.0],
+		     [ABC[1]*math.cos(abc[2]),ABC[1]*math.sin(abc[2]),                                                                           0.0],
+		     [ABC[2]*math.cos(abc[1]),ABC[2]*math.cos(abc[0]),math.sqrt(ABC[2]**2-(ABC[2]*math.cos(abc[1]))**2-(ABC[2]*math.cos(abc[0]))**2)]]) #check this part
 
-#convert in fractionals
-from numpy.linalg import inv
-invcell=inv(cell)
-fract=[]
-for i in range(0,natoms):
+
+
+if 'fract' in locals(): #convert in cartesian
+  print
+  print " ...converting fractional coordinates in cartesian"
+  xyz=[] 
+  for i in range(0,natoms):
+	x=fract[i][0]*cell.item((0,0))+fract[i][1]*cell.item((1,0))+fract[i][2]*cell.item((2,0))
+	y=fract[i][1]*cell.item((0,1))+fract[i][1]*cell.item((1,1))+fract[i][2]*cell.item((2,1))
+	z=fract[i][2]*cell.item((0,2))+fract[i][1]*cell.item((1,2))+fract[i][2]*cell.item((2,2))
+	xyz.append([x,y,z])
+elif 'xyz' in locals(): #convert in fractionals
+  print
+  print " ...converting cartesian coordinates in fractional"
+  from numpy.linalg import inv
+  invcell=inv(cell)
+  fract=[]
+  for i in range(0,natoms):
 	x=xyz[i][0]*invcell.item((0,0))+xyz[i][1]*invcell.item((1,0))+xyz[i][2]*invcell.item((2,0))
 	y=xyz[i][1]*invcell.item((0,1))+xyz[i][1]*invcell.item((1,1))+xyz[i][2]*invcell.item((2,1))
 	z=xyz[i][2]*invcell.item((0,2))+xyz[i][1]*invcell.item((1,2))+xyz[i][2]*invcell.item((2,2))
@@ -248,7 +274,7 @@ if outputformat=="cif":
 	print >> ofile, "_atom_site_charge"
 
 	for i in range(0,natoms):	
-        	label=atom[i]+ str(i+1)
+        	label=atom[i]    #removed: label=atom[i]+"_"+str(i+1)
 		print >> ofile, ('{0:10} {1:5} {2:>9.3f} {3:>9.3f} {4:>9.3f} {5:>9.5f}'.format(label,  atom[i], fract[i][0], fract[i][1], fract[i][2], charge[i]))
 
 
@@ -268,4 +294,12 @@ if outputformat=="cssr":
 	for i in range(0,natoms):
 		print >> ofile, "%4d %3s %8.3f %8.3f %8.3f    0  0  0  0  0  0  0  0  0.000"    %(i+1, atom[i], fract[i][0],fract[i][1],fract[i][2])
  
+
+if outputformat=="xyz":
+   	print >> ofile, "%d"   %(natoms)
+	print >> ofile, "CELL: %.5f  %.5f  %.5f  %.3f  %.3f  %.3f  " %(ABC[0],ABC[1],ABC[2],math.degrees(abc[0]),math.degrees(abc[1]),math.degrees(abc[2]))
+
+	for i in range(0,natoms):
+		print >> ofile, "%3s %8.3f %8.3f %8.3f "  %(atom[i], xyz[i][0],xyz[i][1],xyz[i][2])
+
 
