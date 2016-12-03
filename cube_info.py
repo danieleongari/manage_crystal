28e66e4dc644473f50dfcce9b34f226ffa272178
Daniele Ongari <daniele.ongari@gmail.com>
2016-12-03 21:33:37 +0100

    added calculation of Henry coeff

--- cube_info.py
+++ cube_info.py
@@ -25,35 +25,77 @@ BOHR2ANGS=1./1.88973
 
 ############################################################################## INPUT
 
+shift  =[0]*3
+npoints=[0]*3
+binsize=[0]*3 #works only with orthogonal cells 
+
+
 #reading input file: name and format
 for nfile in range(1,len(sys.argv)):
         print
         print sys.argv[nfile]
 	file = open(sys.argv[nfile],'r')
 	header1 = file.readline()
-	header2 = file.readline()
+	header2 = file.readline().split()
+        shift[0]=float(header2[22])
+	shift[1]=float(header2[23])
+	shift[2]=float(header2[24])
+
 	data = file.readline().split()
 	nat = int(data[0])
 	orig = [ float(data[1])/ANGS2BOHR, float(data[2])/ANGS2BOHR, float(data[3])/ANGS2BOHR ]
-	data = file.readline().split() # X
-	data = file.readline().split() # Y
-	data = file.readline().split() # Z
 
-	for i in range(1,nat):
+	data = file.readline().split() # X
+        npoints[0]=float(data[0])
+        binsize[0]=float(data[1])/ANGS2BOHR                          
+	data = file.readline().split() # Y
+        npoints[1]=float(data[0])
+        binsize[1]=float(data[2])/ANGS2BOHR
+	data = file.readline().split() # Z
+        npoints[2]=float(data[0])
+        binsize[2]=float(data[3])/ANGS2BOHR
+
+	for i in range(0,nat):
 	  data = file.readline().split()
 
 	gridValues=[]
+        count=-1
+        x=0
+        y=0
+        z=0
 
 	while True:
 	 data = file.readline().split()
 	 if len(data)==0:           #if the file is finished stop  
 	    break
-	 gridValues.extend(data)
+         for gridvalue in data:
+           count=count+1;
+           z=orig[2]+(count % npoints[2])*binsize[2]
+           y=orig[1]+math.floor(count/npoints[2]) % npoints[1] * binsize[1]
+           x=orig[0]+math.floor(count/npoints[1]/npoints[2]) % npoints[0] * binsize[0]
+ 
+           if (x>orig[0]+shift[0]) and (y>orig[1]+shift[1]) and (z>orig[2]+shift[2]):
+           	gridvalue=float(gridvalue)
+           	#print "%f %f %f %f" %(x,y,z,gridvalue)
+	   	gridValues.append(gridvalue)		#(kJ)
 
 	gridValues=[float(i) for i in gridValues]
 
-	print "MIN: %.3f " %min(gridValues)
-        print "MAX: %.3f " %max(gridValues)
+	print "MIN (kJ/mol) = %.3f " %min(gridValues)
+        print "MAX (kJ/mol) = %.3f " %max(gridValues)
+
+        #Henry coefficient
+        T= 298       					#K
+        R= 0.008314  					#kJ/mol/K
+        beta=1/R/T;  					#mol/kJ
+
+        boltz_coeff=[ math.exp(-beta*U) for U in gridValues]
+        boltz_ave=sum(boltz_coeff)/len(gridValues)
+        mu_ex=-1/beta*math.log(boltz_ave)
+        K_H=beta*boltz_ave                          	#(mol/kJ)
+        print "mu_ex (kJ/mol) = %.3f "                         %mu_ex
+        print "K_H (mol/Pa/kg) = %.3f (mol/J) * rho_framework (kg/m3)"  %(K_H/1000)		
+        
 
         file.close()
 
