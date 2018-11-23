1106b6505689ea9a28365abbe0d6d8d318655ec7
danieleongari <daniele.ongari@gmail.com>
2018-11-24 00:56:09 +0100

    moving to classes and functions (part1)

--- manage_crystal/__init__.py
+++ manage_crystal/__init__.py
@@ -1,4 +1,8 @@
 import numpy as np
+import re  #re.split(r'(\d+)',"Cu23") = ['Cu', '23', '']
+import math
+from collections import Counter #makes a dictionary
+from manage_crystal.atomic_data import atomic_symbol
 
 class Crys:
     """ Crystal object """
@@ -7,19 +11,28 @@ class Crys:
         self.natom = 0       # number of atoms
         self.atom_type = []  # atomic type: e.g., Cu1
         self.atom_element = []
-        self.inpcoord_xyz = False
-        self.inpcoord_fract = False
+        self.inp_xyz = False
+        self.inp_fract = False
         self.atom_xyz = []   # cartesian coordinates
         self.atom_fract = []
-
-class Cell:
-    def __init__(self):
+        self.atom_charge = []
+        self.tot_charge = []
         self.inp_lengths_angles = False
         self.inp_matrix = False
         self.length = np.zeros(3)
         self.angle_deg = np.zeros(3)
         self.angle_rad = np.zeros(3)
         self.matrix = np.zeros((3,3))
+    def after_parse(self):
+        self.natom = len(self.atom_type)
+        self.atom_element = [ re.split(r'(\d+)',x)[0] for x in self.atom_type ]
+        self.element_count = Counter(self.atom_type)
+        self.atom_atnum = [ atomic_symbol.index(x) for x in self.atom_element ]
+        if all(x ==0 for x in self.angle_rad):
+            self.angle_rad = [math.radians(i) for i in self.angle_deg]
+        else:
+            self.angle_deg = [math.degrees(i) for i in self.angle_rad]
+
     def compute_la_from_matrix(self):
         self.length[0] = math.sqrt(self.matrix[0][0]*self.matrix[0][0]+self.matrix[0][1]*self.matrix[0][1]+self.matrix[0][2]*self.matrix[0][2])
         self.length[1] = math.sqrt(self.matrix[1][0]*self.matrix[1][0]+self.matrix[1][1]*self.matrix[1][1]+self.matrix[1][2]*self.matrix[1][2])
@@ -29,10 +42,6 @@ class Cell:
         self.angle_rad[2] = math.acos((self.matrix[0][0]*self.matrix[1][0]+self.matrix[0][1]*self.matrix[1][1]+self.matrix[0][2]*self.matrix[1][2])/self.length[0]/self.length[1]) #gamma=A^B
         self.angle_deg = [math.degrees(i) for i in self.angle_rad]
     def compute_matrix_from_la(self):
-        if self.angle_rad == np.zeros(3):
-            self.angle_rad = [math.radians(i) for i in self.angle_deg]
-        else:
-            self.angle_deg = [math.degrees(i) for i in self.angle_rad]
         self.matrix[0][0] = self.length[0]
         self.matrix[0][1] = 0.0
         self.matrix[0][2] = 0.0
---
+++ manage_crystal/file_parser.py
@@ -0,0 +1,110 @@
+from manage_crystal import Crys
+
+def parse_cif(file):
+    ''' Parse .cif file and return Crys object '''
+    # REQUIREMENTS:
+    # - only valid for P1 symmetry
+    # - cell data should be specified before the atom data
+    # - if some other "_atom_something" are specified before, it does not work
+    # - after the atom coordinates it breaks with "loop_" or EOF
+    c = Crys()
+    c.inp_lengths_angles = True
+    while True:
+        line = file.readline()
+        data = line.split()
+        if line == "":
+            break
+        if len(data) > 0 and (data[0] == "_cell_length_a"):
+            c.length[0] = float(data[1])
+        if len(data) > 0 and (data[0] == "_cell_length_b"):
+            c.length[1] = float(data[1])
+        if len(data) > 0 and (data[0] == "_cell_length_c"):
+            c.length[2] = float(data[1])
+        if len(data) > 0 and (data[0] == "_cell_angle_alpha"):
+            c.angle_deg[0] = float(data[1])
+        if len(data) > 0 and (data[0] == "_cell_angle_beta"):
+            c.angle_deg[1] = float(data[1])
+        if len(data) > 0 and (data[0] == "_cell_angle_gamma"):
+            c.angle_deg[2] = float(data[1])
+        # if the "_atom_site_***" section starts, remember the order
+        if len(data) > 0 \
+           and len(data[0].split("_")) > 1 \
+           and data[0].split("_")[1] == "atom":
+            data_order_dic = {}
+            order = 0
+            while len(data[0].split("_")) > 1 \
+                  and data[0].split("_")[1] == "atom":
+                data_order_dic[data[0]] = order
+                line = file.readline()
+                data = line.split()
+                order += 1
+            break  #go in the loop to read coordinates
+    # Read atomic element, coordinates and charges
+    if "_atom_site_fract_x" in data_order_dic:
+        c.inp_fract = True
+    elif "_atom_site_Cartn_x" in data_order_dic:
+        c.inp_xyz = True
+    while True:
+        if line == "" \
+           or line =="\n" \
+           or data[0] == "loop_" \
+           or data[0] == "_loop":
+            break
+        # looks for "type_symbol" before and, if missing for "label"
+        if "_atom_site_type_symbol" in data_order_dic:
+            c.atom_type.append(
+                data[data_order_dic["_atom_site_type_symbol"]])
+        elif "_atom_site_label" in data_order_dic:
+            c.atom_type.append(
+                data[data_order_dic["_atom_site_label"]])
+        else:
+            sys.exit("EXIT: in cif missing type_symbol and label")
+        if "_atom_site_fract_x" in data_order_dic:
+            c.atom_fract.append([
+                float(data[data_order_dic["_atom_site_fract_x"]]),
+                float(data[data_order_dic["_atom_site_fract_y"]]),
+                float(data[data_order_dic["_atom_site_fract_z"]]),
+            ])
+        elif "_atom_site_Cartn_x" in data_order_dic:
+            c.atom_xyz.append([
+                float(data[data_order_dic["_atom_site_Cartn_x"]]),
+                float(data[data_order_dic["_atom_site_Cartn_y"]]),
+                float(data[data_order_dic["_atom_site_Cartn_z"]]),
+            ])
+        else:
+            sys.exit("EXIT: in cif missing fract_ and Cartn_ coordinates")
+        if "_atom_site_charge" in data_order_dic:
+            c.atom_charge.append(
+                float(data[data_order_dic["_atom_site_charge"]]))
+        line = file.readline()
+        data = line.split()
+    return c
+
+def parse_pdb(file):
+    ''' Parse .pdb file and return Crys object '''
+    c = Crys()
+    c.inp_xyz = True
+    while True:
+        line = file.readline()
+        data = line.split()
+        if line == "":
+            break
+        elif len(data) > 0 and (data[0] == 'END' or data[0] == 'ENDMDL'):
+            break
+        elif len(data) > 0 and data[0] == 'CRYST1':
+            c.inp_lengths_angles = True
+            c.length = [float(line[0o6:15]),
+                        float(line[15:24]),
+                        float(line[24:33])
+                        ]
+            c.angle_deg = [float(line[33:40]),
+                           float(line[40:47]),
+                           float(line[47:54])
+                           ]
+        elif len(data) > 0 and (data[0] == "ATOM" or data[0] == "HETATM"):
+            c.atom_type.append(data[2]) #maybe read to data[-1]
+            c.atom_xyz.append(
+                [float(line[30:38]),
+                 float(line[38:46]),
+                 float(line[46:54])])
+    return c
