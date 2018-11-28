a1944259f3098b5a0e7ed8714c79022cba2c3477
Daniele Ongari <daniele.ongari@gmail.com>
2018-11-28 18:58:46 +0100

    cell allignement done, TO TEST

--- manage_crystal/__init__.py
+++ manage_crystal/__init__.py
@@ -36,17 +36,39 @@ class Crys:
         self.angle_rad = [0.0] * 3  # Use ONLY when necessary to simplify math
         self.matrix = [[0.0] * 3 for i in range(3)]
 
+    def check_matrix_allign(self):
+        """ Checks if the cell matrix is alligned with xyz axis """
+        # This is important because in an MD the cell can get disalligned and,
+        # if atomic coordinates are cartesian (xyz), cell and coordinates would
+        # be referred to difference reference systems.
+        if all(x == 0 for x in
+               [self.matrix[0][1], self.matrix[0][2], self.matrix[1][2]]):
+            self.matrix_alligned = True
+        else:
+            self.matrix_alligned = False
+
     def check_parse(self):
-        # Input data check (coordinates and cell)
-        if len(self.atom_xyz) > 0: self.inp_xyz = True
-        if len(self.atom_fract) > 0: self.inp_fract = True
+        """ Checks input coordinates and cell"""\
+        # Check atomic coordinates
+
+        if len(self.atom_xyz) > 0:
+            self.inp_xyz = True
+        if len(self.atom_fract) > 0:
+            self.inp_fract = True
         if self.inp_xyz and self.inp_fract:
             sys.exit("WARNING: the input contains both Cartesian and \
                       fractional coordinates. EXIT.")
         elif not self.inp_xyz and not self.inp_fract:
             sys.exit("WARNING: no input coordinates. EXIT.")
-        if all(x != 0 for x in self.length): self.inp_lengths_angles = True
-        if any(x != 0 for x in self.matrix[0]): self.inp_matrix = True
+        # Check input cell
+        if any(x != 0 for x in self.length):
+            self.inp_lengths_angles = True
+            self.matrix_alligned = True
+            if any(x == 0 for x in self.length):
+                sys.exit("WARNING: found cell length equal to zero. EXIT.")
+        if any(x != 0 for x in self.matrix[0]):
+            self.inp_matrix = True
+            self.check_matrix_allign()
         if self.inp_lengths_angles and self.inp_matrix:
             sys.exit("WARNING: the input contains both lengths & angles \
                       and cell matrix. EXIT.")
@@ -131,6 +153,15 @@ class Crys:
         if self.inp_xyz: self.compute_fract_from_xyz()
         elif self.inp_fract: self.compute_xyz_from_fract()
 
+    def fix_cell_notalligned(self):
+        """ Fix the problem of not alligned cell and cartesian coordinates """
+        # matrix > la > matrix(alligned)
+        # atom_fract are still valid
+        # atom_xyz should be overwritten by the atomic_fract in the new cell
+        if self.inp_matrix and self.inp_fract and not self.matrix_alligned:
+            self.compute_matrix_from_la()
+            self.compute_xyz_from_fract()
+
     def transl_coord(self, transl):
         """ Translate the coordinates of the crystal by a defined amount """
         for i in range(self.natom):
@@ -157,9 +188,7 @@ class Crys:
                 self.atom_xyz[i][0], self.atom_xyz[i][1], \
                  self.atom_xyz[i][2] = self.atom_xyz[i][2], \
                   self.atom_xyz[i][0], self.atom_xyz[i][1]
-                self.atom_fract[i][0], self.atom_fract[i][1], \
-                 self.atom_fract[i][2] = self.atom_fract[i][2], \
-                  self.atom_fract[i][0], self.atom_fract[i][1]
+
         else:
             self.length[0], self.length[1], self.length[2] = \
              self.length[1], self.length[2], self.length[0]
@@ -169,9 +198,8 @@ class Crys:
                 self.atom_xyz[i][0], self.atom_xyz[i][1], \
                  self.atom_xyz[i][2] = self.atom_xyz[i][1], \
                   self.atom_xyz[i][2], self.atom_xyz[i][0]
-                self.atom_fract[i][0], self.atom_fract[i][1], \
-                 self.atom_fract[i][2] = self.atom_fract[i][1], \
-                  self.atom_fract[i][2], self.atom_fract[i][0]
+        self.compute_matrix_from_la()
+        self.compute_fract_from_xyz()
 
     def compute_perp_width(self):
         """ Compute perpendicular widths in the cell """
